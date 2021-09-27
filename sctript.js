9f7e404bb872041405b1db51dbe48e5cd6206fb0
Fasil <fasil17062002@gmail.com>
2021-09-27 21:31:44 +0530

    first commit

---
+++ sctript.js
@@ -0,0 +1,27 @@
+
+function video1(){
+    document.getElementById("video-img-1").style.display="none";
+    let video=document.getElementById("video-1").style.display="block";
+    video.src+="?autoplay=1"
+}
+function video2(){
+    document.getElementById("video-img-2").style.display="none";
+    let video=document.getElementById("video-2").style.display="block"; 
+    video.src+="?autoplay=1"
+}
+function video3(){
+    document.getElementById("video-img-3").style.display="none";
+    let video=document.getElementById("video-3").style.display="block"; 
+    video.src+="?autoplay=1"
+}
+function video4(){
+  document.getElementById("video-img-4").style.display="none";
+  let video=document.getElementById("video-4").style.display="block"; 
+  video.src+="?autoplay=1"
+}
+function video5(){
+  document.getElementById("video-img-5").style.display="none";
+  let video=document.getElementById("video-5").style.display="block"; 
+  video.src+="?autoplay=1"
+}
+
